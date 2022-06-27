f07152a9b3071473a5418c8f9039f2f26b8df50c
Reenly <69853981+Reenly@users.noreply.github.com>
2022-06-27 17:36:44 +0300

    Backup scripting & behavior

---
+++ Assets/Scripts/ServerBackup.cs
@@ -0,0 +1,134 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+[System.Serializable]
+public class BackupData
+{
+    [SerializeField]
+    public int ActionsPerformed;
+    public string Date;
+    public List<CardValueType> GameDeck = new List<CardValueType>();
+    public List<string> playerActions = new List<string>();
+    public List<int> indexParameters = new List<int>();
+}
+public class ServerBackup : MonoBehaviour
+{
+    private string fileDataPath;
+    [SerializeField] private BackupData DataHold = new BackupData();
+    private void Awake()
+    {
+        //makesSureSavefilesFolderAlwaysExists
+        CheckFolderDataPath();
+    }
+    private void Start()
+    {
+        PerformBackup();
+    }
+    #region BackupFunctions
+    private void PerformBackup()
+    {
+        GetFileDataPath();
+
+        DataHold.ActionsPerformed = DataHold.playerActions.Count;
+        DataHold.Date = DateTime.Now.ToString("G");
+
+        string JsonOutput = JsonUtility.ToJson(DataHold, true);
+        System.IO.File.WriteAllText(fileDataPath, JsonOutput);
+
+        Debug.Log($"Backup location: {fileDataPath}, Backup date: {DataHold.Date}");
+    }
+    public void ReadBackup(string inputToFile)
+    {
+        if (inputToFile.Length == 9)
+        {
+            inputToFile = ReadFileDataPath(inputToFile);
+            if (File.Exists(inputToFile))
+            {
+                string JsonInput = System.IO.File.ReadAllText(inputToFile);
+                JsonUtility.FromJsonOverwrite(JsonInput, DataHold);
+            }
+            else
+            {
+                Debug.LogError("ReadBackup >> File does not exist");
+            }
+        }
+        else
+        {
+            Debug.LogError("ReadBackup >> Invalid Input");
+        }
+    }
+    #endregion
+    #region ChecksAndRandomGeneration
+    public void BackupDeck(List<CardValueType> deckBackup)
+    {
+        DataHold.GameDeck = deckBackup;
+    } 
+        
+    private string generateRandomSaveId()
+    {
+        string RandomToReturn = "";
+        for (int i = 0; i < 9; i++)
+        {
+            RandomToReturn += UnityEngine.Random.Range(0, 9).ToString();
+        }
+        return RandomToReturn;
+    }
+    private void CheckFolderDataPath()
+    {
+        string path = getFolderDataPath();
+        if (Directory.Exists(path))
+        {
+            Debug.Log("FolderFound");
+            //doNothing
+        }
+        else
+        {
+            Directory.CreateDirectory(path);
+        }
+    }
+    #endregion
+    #region FilesLocations
+    private string ReadFileDataPath(string textInput)
+    {
+#if UNITY_EDITOR
+        return Application.dataPath + "/SaveFiles/" + textInput + ".txt";
+#elif UNITY_ANDROID
+        return Application.persistentDataPath + "/SaveFiles/" + textInput + ".txt";
+#elif UNITY_IPHONE
+        return Application.persistentDataPath + "/SaveFiles/" + textInput + ".txt";
+#else
+        return Application.dataPath + "/SaveFiles/" + textInput + ".txt";
+#endif
+    }
+    private void GetFileDataPath()
+    {
+#if UNITY_EDITOR
+        fileDataPath = Application.dataPath + "/SaveFiles/" + generateRandomSaveId() + ".txt";
+#elif UNITY_ANDROID
+        fileDataPath = Application.persistentDataPath + "/SaveFiles/" + generateRandomSaveId() + ".txt";
+#elif UNITY_IPHONE
+        fileDataPath = Application.persistentDataPath + "/SaveFiles/" + generateRandomSaveId() + ".txt";
+#else
+        fileDataPath = Application.dataPath + "/SaveFiles/" + generateRandomSaveId() + ".txt";
+#endif
+    }
+    private string getFolderDataPath()
+    {
+#if UNITY_EDITOR
+        Debug.Log(Application.dataPath + "/SaveFiles");
+        return Application.dataPath + "/SaveFiles";
+#elif UNITY_ANDROID
+        Debug.Log(Application.persistentDataPath + "/SaveFiles");
+        return Application.persistentDataPath + "/SaveFiles";
+#elif UNITY_IPHONE
+        Debug.Log(Application.persistentDataPath + "/SaveFiles");
+        return Application.persistentDataPath + "/SaveFiles";
+#else
+        Debug.Log(Application.dataPath + "/SaveFiles");
+        return Application.dataPath + "/SaveFiles";
+#endif
+    }
+    #endregion
+}
