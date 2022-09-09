81015aa30b0bf1a460106e382f51632d9096d2c2
cezatul <58083143+cezatul@users.noreply.github.com>
2022-09-09 15:24:25 +0300

    moved animation + random animations

--- Assets/Scripts/Razboi/AnimationControl.cs
+++ Assets/Scripts/Razboi/AnimationControl.cs
@@ -8,30 +8,46 @@ using System.Threading.Tasks;
 [System.Serializable]
 public class AnimList
 {
-    public string AnimationName;
+    public AnimationClip AnimationName;
     public float AnimationDelay;
 }
 public class AnimationControl : MonoBehaviour
 {
     public Animator ObjectAnimator;
     public bool DoIdleAnimation;
+    private bool DoAnimation;
     public int IndexOfAnimationToIdle;
     public List<AnimList> ListOfAnimations = new List<AnimList>();
 
+    private async void Start()
+    {
+       
+        if (DoIdleAnimation)
+        {
+            await StartDelay();
+        }
+    }
     private async void Update()
     {
-        if (DoIdleAnimation)
+        if (DoAnimation)
         {
             await IdleAnimation();
         }
     }
+
+    private async Task StartDelay()
+    {
+        await Task.Delay((int)(Random.Range(0.3f, 2.7f) * 1000));
+        DoAnimation = true;
+    }
     private async Task IdleAnimation()
     {
-        ObjectAnimator.Play(ListOfAnimations[IndexOfAnimationToIdle].AnimationName);
-        await Task.Delay((int)ListOfAnimations[IndexOfAnimationToIdle].AnimationDelay * 1000);
+        ObjectAnimator.Play(ListOfAnimations[IndexOfAnimationToIdle].AnimationName.name);
+        await Task.Delay((int)(ListOfAnimations[IndexOfAnimationToIdle].AnimationDelay + Random.Range(0.5f,2.5f))  * 1000);
     }
     public void PlayAnimationOfIndex(int Index)
     {
-        ObjectAnimator.Play(ListOfAnimations[Index].AnimationName);
+        ObjectAnimator.Play(ListOfAnimations[Index].AnimationName.name);
     }
+
 }
