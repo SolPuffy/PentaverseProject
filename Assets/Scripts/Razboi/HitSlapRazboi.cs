20aa00e2f0a84e34158845bcacd62578d25782e8
chirica-dragos <67731768+chirica-dragos@users.noreply.github.com>
2023-01-13 13:27:03 +0200

    Update HitSlapRazboi.cs

--- Assets/Scripts/Razboi/HitSlapRazboi.cs
+++ Assets/Scripts/Razboi/HitSlapRazboi.cs
@@ -131,7 +131,7 @@ public class HitSlapRazboi : NetworkBehaviour
         while (IndexOfActivePlayer == p.playerIndex)
         {
             HitCards(p.playerIndex, p.Nome);
-            yield return new WaitForSeconds(0.1f);
+            yield return new WaitForSeconds(0.2f);
         }
     }
     IEnumerator Setup()
